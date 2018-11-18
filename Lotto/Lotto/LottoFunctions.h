0796dd474669cc3fa6ae07b16de261a4cc850087
michaelbridgette <c00205948@itcarlow.ie>
2018-11-18 17:14:23 +0000

    Completed Unit test lab

---
+++ Lotto/Lotto/LottoFunctions.h
@@ -0,0 +1,70 @@
+
+#include <vector>
+#include<iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h> 
+
+//check if number is between 1 and 46 inclusive
+bool numCheck(int number)
+{
+	if (number <= 46 && number >= 1)
+	{
+		return true;
+	}
+	else
+	{
+		std::cout << "Number not between 1 and 46" << std::endl;
+		return false;
+	}
+}
+
+bool checkForDuplicate(int number, std::vector<int> &collection)
+{
+	/*if (collection.size() == 0)
+	{
+	collection.push_back(number);
+	return false;
+	}*/
+
+	for (int i = 0; i < collection.size(); i++)
+	{
+		if (collection.at(i) == number)
+		{
+			return true;
+		}
+	}
+	collection.push_back(number);
+
+	return false;
+}
+
+void generateLottoNumbers(std::vector<int> & lotto)
+{
+	srand(time(NULL));
+
+	while (lotto.size() < 6)
+	{
+		checkForDuplicate(rand() % 46 + 1, lotto);
+	}
+	std::cout << "The lotto numbers are: ";
+	for (int i = 0; i < lotto.size(); i++)
+	{
+		std::cout << lotto.at(i) << " ";
+	}
+}
+
+int getMatches(std::vector<int> numbers, std::vector<int> lotto)
+{
+	int matches = 0;
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		for (int j = 0; j < lotto.size(); j++)
+		{
+			if (numbers.at(i) == lotto.at(j))
+			{
+				matches++;
+			}
+		}
+	}
+	return matches;
+}
---
+++ Lotto/UnitTest1/unittest1.cpp
@@ -0,0 +1,96 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include"../Lotto/LottoFunctions.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestForDuplicate)
+		{
+			// TODO: Your test code here
+			std::vector<int> coll;
+			coll.push_back(1);
+
+			Assert::AreEqual(checkForDuplicate(1, coll), true);
+		}
+		TEST_METHOD(TestForDuplicate2)
+		{
+			// TODO: Your test code here
+			std::vector<int> coll;
+			coll.push_back(1);
+			coll.push_back(2);
+			coll.push_back(3);
+
+			Assert::AreEqual(checkForDuplicate(4, coll), false);
+		}
+		TEST_METHOD(NumberBoundaryCheck)
+		{
+			int myNum = 600000000;
+
+			Assert::AreEqual(numCheck(myNum), false);
+		}
+		TEST_METHOD(NumberBoundaryCheck2)
+		{
+			int myNum = -600000000;
+
+			Assert::AreEqual(numCheck(myNum), false);
+		}
+		TEST_METHOD(NumberBoundaryCheck3)
+		{
+			int myNum = 12;
+
+			Assert::AreEqual(numCheck(myNum), true);
+		}
+
+		TEST_METHOD(NumberMatchCheck)
+		{
+			std::vector<int> myNumbers;
+			std::vector<int> lotto;
+
+			myNumbers.push_back(1);
+			myNumbers.push_back(2);
+			myNumbers.push_back(3);
+
+			lotto.push_back(1);
+			lotto.push_back(2);
+			lotto.push_back(3);
+
+			Assert::AreEqual(getMatches(myNumbers, lotto), 3);
+		}
+		TEST_METHOD(NumberMatchCheck2)
+		{
+			std::vector<int> myNumbers;
+			std::vector<int> lotto;
+
+			myNumbers.push_back(1);
+			myNumbers.push_back(2);
+			myNumbers.push_back(3);
+
+			lotto.push_back(4);
+			lotto.push_back(5);
+			lotto.push_back(6);
+
+			Assert::AreEqual(getMatches(myNumbers, lotto), 0);
+		}
+		TEST_METHOD(NumberMatchCheck3)
+		{
+			std::vector<int> myNumbers;
+			std::vector<int> lotto;
+
+			myNumbers.push_back(1);
+			myNumbers.push_back(2);
+			myNumbers.push_back(3);
+
+			lotto.push_back(3);
+			lotto.push_back(4);
+			lotto.push_back(5);
+
+			Assert::AreEqual(getMatches(myNumbers, lotto), 1);
+		}
+	};
+}
